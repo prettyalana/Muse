2ee674a558e6e101b1fcb2c42f258d8c91477312
prettyalana <129349619+prettyalana@users.noreply.github.com>
2024-03-29 14:24:47 -0500

    edit views and generate devise forms

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -22,9 +22,22 @@
 #
 class Offer < ApplicationRecord
   belongs_to :seller, class_name: "User"
-  belongs_to :listing 
+  belongs_to :listing
   belongs_to :message
+
+  #after_update :marked_purchased
 
   # add the counter offer and accepted boolean field to the offers
   # enum status: { pending: "pending", countered: "countered", rejected: "rejected", accepted: "accepted"}
+  #
+  #private
+  #
+  #def marked_purchased
+   # if self.accepted
+    # self.listing.update(
+      # purchased: true
+    #)
+    #end
+  #end
+
 end
