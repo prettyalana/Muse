3b289fd2c6a22047542d420bcb69d5e36dd9458d
prettyalana <129349619+prettyalana@users.noreply.github.com>
2024-04-11 08:40:49 -0500

    Testing issue in the home#show route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find_by!(username: params.fetch(:username))
+    @user = User.find_by(username: params.fetch(:username))
     @listings = Listing.all.order(:created_at => :desc)
   end
 end
