e6b095958644c751f0501fb8ebcc88cadeb94055
prettyalana <129349619+prettyalana@users.noreply.github.com>
2025-03-11 22:31:24 -0500

    Remove default_meta_tags from ApplicationHelper - Create a meta_tags service to encapsulate the logic and keep code DRY - In the meta_tags partial use the service to render the meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,2 @@
 module ApplicationHelper
-  def default_meta_tags
-    {
-      site: "Muse",
-      image: image_url('/assets/Muse-2.png'),
-      description: "An app made for the customer by the customer.",
-      og: {
-        title: "Muse",
-        image: image_url('/assets/Muse-2.png'),
-        description: "An app made for the customer by the customer.",
-        site_name: "muse-shopping.com"
-      }
-      # TODO: add twitter tags
-    }
-  end
 end
