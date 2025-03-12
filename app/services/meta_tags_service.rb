9ec112418e8eed0f6dc39db10654415b64806b9a
prettyalana <129349619+prettyalana@users.noreply.github.com>
2025-03-11 22:50:47 -0500

    Configure sitemap in production and update image in MetaTagsService to assert_url - asset_url is available in production to resolve the full URL of an asset

--- app/services/meta_tags_service.rb
+++ app/services/meta_tags_service.rb
@@ -2,11 +2,11 @@ class MetaTagsService
   def self.default_meta_tags
     {
       site: "Muse",
-      image: Rails.application.assets.url('Muse-2.png'),
+      image: asset_url('Muse-2.png'),
       description: "Muse is a customer-driven app that prioritizes the customer's needs by connecting buyers directly with sellers, merchants, and retailers, and offering a platform to curate personalized shopping lists.",
       og: {
         title: "Muse - A Brand New Customer-Driven Shopping Experience",
-        image: Rails.application.assets.url('Muse-2.png'),
+        image: asset_url('Muse-2.png'),
         description: "The best app to find exactly what you're looking for. Create listings for what you want, and let the sellers come to you. Curate your perfect shopping list and discover products you actually want.",
         site_name: "Muse"
       }
