ddb548467ac4a7eaadc0ff7454ea70e0dc5e2394
prettyalana <129349619+prettyalana@users.noreply.github.com>
2025-03-11 22:57:54 -0500

    Update image in MetaTagsService to use Rails.application.routes.url_helpers for generating URLs for assets

--- app/services/meta_tags_service.rb
+++ app/services/meta_tags_service.rb
@@ -1,12 +1,14 @@
 class MetaTagsService
   def self.default_meta_tags
+    image_url = Rails.application.routes.url_helpers.asset_url('Muse-2.png', host: 'https://muse-shopping.com')
+
     {
       site: "Muse",
-      image: asset_url('Muse-2.png'),
+      image: image_url,
       description: "Muse is a customer-driven app that prioritizes the customer's needs by connecting buyers directly with sellers, merchants, and retailers, and offering a platform to curate personalized shopping lists.",
       og: {
         title: "Muse - A Brand New Customer-Driven Shopping Experience",
-        image: asset_url('Muse-2.png'),
+        image: image_url,
         description: "The best app to find exactly what you're looking for. Create listings for what you want, and let the sellers come to you. Curate your perfect shopping list and discover products you actually want.",
         site_name: "Muse"
       }
